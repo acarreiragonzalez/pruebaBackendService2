521b664af8a519848d2c57263672140afb666ae2
Ángel Carrera González <acarreiragonzalez@danielcastelao.org>
2017-09-19 14:10:17 +0200

    First commit

---
+++ pruebaBackendService2/src/pruebaBackendService2/BackendServiceTest.java
@@ -0,0 +1,30 @@
+package pruebaBackendService2;
+
+public class BackendServiceTest {
+
+	
+	public int return1(){
+		
+		
+		return 1;
+	}
+	
+	public int return2(){
+		
+		
+		return 2;
+	}
+	
+	public int return3(){
+		
+		
+		return 3;
+	}
+	
+	public int return4(){
+		
+		
+		return 4;
+	}
+	
+}
